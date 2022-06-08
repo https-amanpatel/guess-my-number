6075670af5703754369f47d2206fe1bed0fb4fb8
Aman Patel <aman17626@gmail.com>
2022-06-08 20:55:19 +0530

    DRY principle

--- script.js
+++ script.js
@@ -29,28 +29,17 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.highscore').textContent = highScore;
         }
 
-        // When guess is too high
-    } else if (guess > secretNumber) {
+        // When guess is wrong
+    } else if (guess !== secretNumber) {
 
         if (score > 1) {
-            document.querySelector('.message').textContent = '📈 Too high!';
+            document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = '😶 You lost the game!';
             document.querySelector('.score').textContent = 0;
-        }
 
-        // When guess is too low
-    } else if (guess < secretNumber) {
-
-        if (score > 1) {
-            document.querySelector('.message').textContent = '📉 Too low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '😶 You lost the game!';
-            document.querySelector('.score').textContent = 0;
         }
     }
 });
